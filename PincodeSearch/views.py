cab71088d9f22757aa8701f37872b7e68ff0c8a2
nitin099 <coolnitin61@gmail.com>
2018-09-10 14:32:39 +0530

    Updated

--- PincodeSearch/views.py
+++ PincodeSearch/views.py
@@ -37,32 +37,3 @@ def search(request):
 		'form': form,
 	}
 	return render(request, "searchpincode.html", context)
-
-
-# This view uses api to get pincodes.
-
-# def search(request):
-# 	form = SearchForm(request.POST or None)
-# 	if form.is_valid():
-# 		pincode = form.cleaned_data.get('pincode')
-# 		print(pincode)
-# 		url = "http://postalpincode.in/api/pincode/%s" % pincode
-# 		response = requests.get(url)
-# 		result = response.json()
-# 		print(result,"$$$$$$$$$$$$$$$$$")
-# 		print(json.dumps(result, indent=4))
-# 		print (type(result))
-# 		try:
-# 			state = result["PostOffice"][0]["State"]
-# 			district = result["PostOffice"][0]["District"]
-# 			circle = result["PostOffice"][0]["Circle"]
-# 			context = {
-# 			'form': form,
-# 			'state': state,
-# 			"district": district,
-# 			"circle": circle
-# 			}
-# 			return render(request, "searchpincode.html", context)
-# 		except:
-# 			return render(request, "searchpincode.html", {"invalid":"Invalid Pincode"})
-# 	return render(request,'searchpincode.html', {"form": form})
